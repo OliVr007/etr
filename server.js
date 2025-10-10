198e7b99bcf8d68dfe395e7cf87f10a66b407df8
OliVr007 <oliverhd55@gmail.com>
2025-10-10 21:10:49 +0200

    feat: initialize project with Express, Prisma, and EJS
    - Added package.json with dependencies and scripts for development
    - Created Prisma schema for user model
    - Added CSS styles for login page
    - Implemented server.js with session management and login functionality
    - Created EJS views for index and login pages

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const getIronSession = require("iron-session").getIronSession;
+const { PrismaClient } = require("./generated/prisma");
+const { compare } = require("bcrypt");
+
+const app = express();
+const port = 3000;
+
+const SESSION_COOKIE = "session";
+const SESSION_PASSWORD = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+const db = new PrismaClient();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded());
+
+app.use(async (req, res, next) => {
+	req.session = await getIronSession(req, res, { password: SESSION_PASSWORD, cookieName: SESSION_COOKIE });
+
+	await next();
+});
+
+app.get("/", async (req, res) => {
+	if (!req.session.id) return res.redirect("/login");
+
+	res.render("index", {
+		user_name: req.session.user_name,
+	});
+});
+
+app.get("/login", async (req, res) => {
+	if (req.session.id) return res.redirect("/");
+	res.render("login");
+});
+
+app.post("/api/login", async (req, res) => {
+	console.log(req.body);
+
+	const data = await db.users.findFirst({
+		where: {
+			user_name: req.body.username,
+		},
+	});
+
+	if (!data) return redirect("/login");
+
+	if (await compare(req.body.password, data.password)) {
+		req.session.id = data.id;
+		req.session.user_name = data.user_name;
+		await req.session.save();
+	}
+
+	return res.redirect("/");
+});
+
+app.get("/logout", async (req, res) => {
+	await req.session.destroy();
+
+	return res.redirect("/login");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
